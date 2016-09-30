722e1c4fa80630968f7c0f2ffbb6fbe3404fd30c
Johan van der Wijk <johan@vanderwijk.com>
2016-09-30 15:23:42 +0200

    Nieuwe homepage

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 <div class="row main flex">
 
-	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Home Intro')) : endif; ?>
+	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Home Intro') ) : endif; ?>
 
 	<div class="col two-thirds">
 		<div class="block">
@@ -35,16 +35,16 @@
 
 	<div class="col one-third">
 		<div class="block">
-			<a href="" class="category nieuws">Nieuws</a>
+			<a href="/categorie/artikelen/" class="category artikelen">Artikelen</a>
 
 			<?php
-				$news_args = array(
+				$article_args = array(
 					'numberposts' => 2,
 					'category' => 24
 				);
-				$news_posts = get_posts( $news_args );
+				$article_posts = get_posts( $article_args );
 
-				foreach( $news_posts as $post) {
+				foreach( $article_posts as $post ) {
 					setup_postdata( $post );
 					echo '<a href="' . get_permalink() . '">';
 					the_post_thumbnail( 'thumbnail' );
@@ -60,7 +60,7 @@
 				}
 				wp_reset_postdata(); ?>
 
-			<p><strong><a href="/categorie/nieuws/">Bekijk hier meer nieuws >></a></strong></p>
+			<p><strong><a href="/categorie/artikelen/">Bekijk hier meer artikelen >></a></strong></p>
 		</div>
 	</div>
 
