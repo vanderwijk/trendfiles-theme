d61809b2bf731fed329720e4af46379fba75dbb0
Johan van der Wijk <vanderwijk@users.noreply.github.com>
2017-11-10 20:20:19 +0100

    Tooltip on right

--- js/prognoses-wervingsbehoefte-stap-1.js
+++ js/prognoses-wervingsbehoefte-stap-1.js
@@ -93,7 +93,7 @@ function maakGrafiek(regio, functie) {
 
 	jQuery('.tooltip:not(.tooltipstered)').tooltipster({
 		'delay': 50,
-		'side': 'top'
+		'side': 'right'
 	});
 
 	// Kleur regio's aan de hand van kleuren json
