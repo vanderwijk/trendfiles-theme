9709937dccfb336f23077849f865d1f9f2fcb6d6
Johan van der Wijk <johan@vanderwijk.nl>
2020-02-12 16:01:37 +0100

    Buttons

--- templates/factsheet-ti-installatie.php
+++ templates/factsheet-ti-installatie.php
@@ -8,6 +8,19 @@ get_header(); ?>
 		<div class="col">
 			<header class="block">
 				<h1 class="category">Technische Installatiebranche</h1>
+				<ul class="knoppen">
+					<li id="nederland">Nederland</li>
+					<li class="disabled" id="noord-nederland">Noord Nederland</li>
+					<li class="disabled" id="noord-holland">Noord-Holland</li>
+					<li class="disabled" id="midden-nederland">Midden Nederland</li>
+					<li class="disabled" id="gelderland-overijssel">Gelderland/Overijssel</li>
+					<li class="disabled" id="nederland">Nederland</li>
+					<li class="disabled" id="zuid-nederland">Zuid Nederland</li>
+					<li class="disabled" id="zuid-holland">Zuid-Holland</li>
+				</ul>
+				<ul class="knoppen pdf">
+					<li><a href="/wp-content/themes/trendfiles-theme/pdf/factsheet-technischeinstallatiebranche-nederland.pdf">PDF / Printversie</a></li>
+				</ul>
 			</header>
 		</div>
 	</div>
@@ -24,24 +37,24 @@ get_header(); ?>
 
 <script>
 var panZoomTiger = svgPanZoom('#factsheet-ti-installatie', {
- panEnabled: true
-, controlIconsEnabled: true
-, zoomEnabled: true
-, dblClickZoomEnabled: true
-, mouseWheelZoomEnabled: false
-, preventMouseEventsDefault: true
-, zoomScaleSensitivity: 0.2
-, minZoom: 1
-, maxZoom: 10
-, fit: true
-, contain: false
-, center: true
-, refreshRate: 'auto'
-, beforeZoom: function(){}
-, onZoom: function(){}
-, beforePan: function(){}
-, onPan: function(){}
-, onUpdatedCTM: function(){}
-, eventsListenerElement: null
+	panEnabled: true
+	, controlIconsEnabled: true
+	, zoomEnabled: true
+	, dblClickZoomEnabled: true
+	, mouseWheelZoomEnabled: false
+	, preventMouseEventsDefault: true
+	, zoomScaleSensitivity: 0.2
+	, minZoom: 1
+	, maxZoom: 10
+	, fit: true
+	, contain: false
+	, center: true
+	, refreshRate: 'auto'
+	, beforeZoom: function(){}
+	, onZoom: function(){}
+	, beforePan: function(){}
+	, onPan: function(){}
+	, onUpdatedCTM: function(){}
+	, eventsListenerElement: null
 });
 </script>
