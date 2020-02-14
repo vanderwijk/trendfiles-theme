21201c4f503c45b8b19831e1901a2c51d7db4054
Johan van der Wijk <johan@vanderwijk.nl>
2020-02-14 15:03:17 +0100

    Aantal werknemers

--- js/factsheet-technische-installatiebranche.js
+++ js/factsheet-technische-installatiebranche.js
@@ -99,14 +99,44 @@ function maakGrafiek(regio) {
 	
 	})
 
-	jQuery('#download-pdf').attr('href', '/wp-content/themes/trendfiles-theme/pdf/factsheet_technischeinstallatiebranche_' + regio + '.pdf');
+	document.getElementById('download-pdf').setAttribute('href', '/wp-content/themes/trendfiles-theme/pdf/factsheet_technischeinstallatiebranche_' + regio + '.pdf');
 
-	jQuery('#vulling_aantal_bedrijven_per_jaar_2018').attr('x2', x2_aantal_bedrijven_per_jaar_2018);
-	jQuery('#vulling_aantal_bedrijven_per_jaar_2017').attr('x2', x2_aantal_bedrijven_per_jaar_2017);
-	jQuery('#vulling_aantal_bedrijven_per_jaar_2016').attr('x2', x2_aantal_bedrijven_per_jaar_2016);
+	document.getElementById('vulling_aantal_bedrijven_per_jaar_2018').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2018);
+	document.getElementById('vulling_aantal_bedrijven_per_jaar_2017').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2017);
+	document.getElementById('vulling_aantal_bedrijven_per_jaar_2016').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2016);
 
-	jQuery('#aantal_bedrijven_per_jaar_2018').attr('x', x2_aantal_bedrijven_per_jaar_2018 + 10);
-	jQuery('#aantal_bedrijven_per_jaar_2017').attr('x', x2_aantal_bedrijven_per_jaar_2017 + 10);
-	jQuery('#aantal_bedrijven_per_jaar_2016').attr('x', x2_aantal_bedrijven_per_jaar_2016 + 10);
+	document.getElementById('aantal_bedrijven_per_jaar_2018').setAttribute('x', x2_aantal_bedrijven_per_jaar_2018 + 10);
+	document.getElementById('aantal_bedrijven_per_jaar_2017').setAttribute('x', x2_aantal_bedrijven_per_jaar_2017 + 10);
+	document.getElementById('aantal_bedrijven_per_jaar_2016').setAttribute('x', x2_aantal_bedrijven_per_jaar_2016 + 10);
+
+
+	var aantal_werknemers_per_jaar = gegevens[regio].aantal_werknemers_per_jaar.jaar_2019;
+	var aantal_werknemers_per_jaar_2018 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2018;
+	var aantal_werknemers_per_jaar_2017 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2017;
+	var aantal_werknemers_per_jaar_2016 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2016;
+	
+	var x2_aantal_werknemers_per_jaar_2018 = aantal_werknemers_per_jaar_2018 * 487 / aantal_werknemers_per_jaar;
+	var x2_aantal_werknemers_per_jaar_2017 = aantal_werknemers_per_jaar_2017 * 487 / aantal_werknemers_per_jaar;
+	var x2_aantal_werknemers_per_jaar_2016 = aantal_werknemers_per_jaar_2016 * 487 / aantal_werknemers_per_jaar;
+	
+	var aantal_werknemers_per_kwartaal_1 = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_1;
+	var aantal_werknemers_per_kwartaal_2 = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_2;
+	var aantal_werknemers_per_kwartaal_3 = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_3;
+	var aantal_werknemers_per_kwartaal_4 = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_4;
+
+	document.getElementById('aantal_werknemers').textContent = formatNumber(aantal_werknemers_per_jaar);
+
+	document.getElementById('vulling_aantal_werknemers_per_jaar_2018').setAttribute('x2', x2_aantal_werknemers_per_jaar_2018);
+	document.getElementById('vulling_aantal_werknemers_per_jaar_2017').setAttribute('x2', x2_aantal_werknemers_per_jaar_2017);
+	document.getElementById('vulling_aantal_werknemers_per_jaar_2016').setAttribute('x2', x2_aantal_werknemers_per_jaar_2016);
+
+	document.getElementById('aantal_werknemers_per_jaar_2018').setAttribute('x', x2_aantal_werknemers_per_jaar_2018 + 10);
+	document.getElementById('aantal_werknemers_per_jaar_2017').setAttribute('x', x2_aantal_werknemers_per_jaar_2017 + 10);
+	document.getElementById('aantal_werknemers_per_jaar_2016').setAttribute('x', x2_aantal_werknemers_per_jaar_2016 + 10);
+
+	document.getElementById("aantal_werknemers_per_kwartaal_1").textContent = formatNumber(aantal_werknemers_per_kwartaal_1);
+	document.getElementById("aantal_werknemers_per_kwartaal_2").textContent = formatNumber(aantal_werknemers_per_kwartaal_2);
+	document.getElementById("aantal_werknemers_per_kwartaal_3").textContent = formatNumber(aantal_werknemers_per_kwartaal_3);
+	document.getElementById("aantal_werknemers_per_kwartaal_4").textContent = formatNumber(aantal_werknemers_per_kwartaal_4);
 
 }
