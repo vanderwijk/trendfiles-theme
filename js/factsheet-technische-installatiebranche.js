1820245b49b5033b66417376f9044bf8efbbbbdc
Johan van der Wijk <johan@vanderwijk.nl>
2020-02-20 21:36:00 +0100

    Cirlcle

--- js/factsheet-technische-installatiebranche.js
+++ js/factsheet-technische-installatiebranche.js
@@ -213,12 +213,39 @@ function maakGrafiek(regio) {
 	var leerbedrijven = gegevens[regio].leerlingen_leerbedrijven.leerbedrijven;
 	var leerlingen_leerbedrijven = gegevens[regio].leerlingen_leerbedrijven.leerlingen;
 
-	document.getElementById('leerbedrijven').textContent = formatNumber(leerbedrijven);
-	document.getElementById('leerlingen_leerbedrijven').textContent = formatNumber(leerlingen_leerbedrijven);
-
 	var opleidingscentra = gegevens[regio].leerlingen_opleidingscentra.opleidingscentra;
 	var leerlingen_opleidingscentra = gegevens[regio].leerlingen_opleidingscentra.leerlingen;
 
+	totaal_leerlingen = leerlingen_opleidingscentra + leerlingen_leerbedrijven;
+
+	var percentage_leerlingen_opleidingscentra = Math.round(leerlingen_opleidingscentra * 100 / totaal_leerlingen);
+	var percentage_leerlingen_leerbedrijven = Math.round(leerlingen_leerbedrijven * 100 / totaal_leerlingen);
+
+	document.getElementById('leerbedrijven').textContent = formatNumber(leerbedrijven);
+	document.getElementById('leerlingen_leerbedrijven').textContent = formatNumber(leerlingen_leerbedrijven);
+
 	document.getElementById('opleidingscentra').textContent = formatNumber(opleidingscentra);
 	document.getElementById('leerlingen_opleidingscentra').textContent = formatNumber(leerlingen_opleidingscentra);
+
+	document.getElementById('percentage_leerlingen_leerbedrijven').textContent = '(' + formatNumber(percentage_leerlingen_leerbedrijven) + '%)';
+	document.getElementById('percentage_leerlingen_opleidingscentra').textContent = '(' + formatNumber(percentage_leerlingen_opleidingscentra) + '%)';
+
+	var techniek_ti = gegevens[regio].opleidingen.techniek_ti;
+	var techniek_overig = gegevens[regio].opleidingen.techniek_overig;
+	var overig = gegevens[regio].opleidingen.overig;
+
+	document.getElementById('techniek_ti').textContent = formatNumber(techniek_ti);
+	document.getElementById('techniek_overig').textContent = formatNumber(techniek_overig);
+	document.getElementById('overig').textContent = formatNumber(overig);
+
+	var element_leerlingen_leerbedrijven = document.getElementById('circle_leerlingen_leerbedrijven');
+	var pathLength_leerlingen = element_leerlingen_leerbedrijven.getTotalLength();
+	stroke_leerlingen_leerbedrijven = leerlingen_leerbedrijven * pathLength_leerlingen / totaal_leerlingen;
+	element_leerlingen_leerbedrijven.style.strokeDasharray = stroke_leerlingen_leerbedrijven;
+/*
+	var element_leerlingen_opleidingscentra = document.getElementById('circle_leerlingen_opleidingscentr');
+	var pathLength_leerlingen = element_leerlingen_opleidingscentr.getTotalLength();
+	stroke_leerlingen_opleidingscentr = leerlingen_opleidingscentr * pathLength_leerlingen / totaal_leerlingen;
+	element_leerlingen_opleidingscentr.style.strokeDasharray = stroke_leerlingen_opleidingscentr;
+*/
 }
