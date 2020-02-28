c11487ab16f88db5ea99597172c98a457b47f0bc
Johan van der Wijk <johan@vanderwijk.nl>
2020-02-28 14:12:06 +0100

    Animation

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-define('TRENDFILES_THEME_VER', '1.1.3');
+define('TRENDFILES_THEME_VER', '1.1.4');
 
 if (isset($_SERVER['HTTPS'])) {
 	$protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https://" : "http://";
