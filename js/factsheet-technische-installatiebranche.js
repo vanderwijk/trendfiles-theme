3857d44196f0a0704773a91f282f46058fe66c98
Johan van der Wijk <johan@vanderwijk.nl>
2020-05-14 14:09:44 +0200

    Factheet technische installatiebranche

--- js/factsheet-technische-installatiebranche.js
+++ js/factsheet-technische-installatiebranche.js
@@ -43,29 +43,34 @@ jQuery(document).ready(function($) {
 function maakGrafiek(regio) {
 	//console.log( gegevens );
 
-	var aantal_bedrijven_per_jaar = gegevens[regio].aantal_bedrijven_per_jaar.jaar_2019;
+	var aantal_bedrijven_per_jaar = gegevens[regio].aantal_bedrijven_per_kwartaal.kwartaal_1;
 
 	document.getElementById('aantal_bedrijven').textContent = formatNumber(aantal_bedrijven_per_jaar);
 
+	var aantal_bedrijven_per_jaar_2019 = gegevens[regio].aantal_bedrijven_per_jaar.jaar_2019;
 	var aantal_bedrijven_per_jaar_2018 = gegevens[regio].aantal_bedrijven_per_jaar.jaar_2018;
 	var aantal_bedrijven_per_jaar_2017 = gegevens[regio].aantal_bedrijven_per_jaar.jaar_2017;
-	var aantal_bedrijven_per_jaar_2016 = gegevens[regio].aantal_bedrijven_per_jaar.jaar_2016;
+	//var aantal_bedrijven_per_jaar_2016 = gegevens[regio].aantal_bedrijven_per_jaar.jaar_2016;
 
+	document.getElementById('aantal_bedrijven_per_jaar_2019').textContent = formatNumber(aantal_bedrijven_per_jaar_2019);
 	document.getElementById('aantal_bedrijven_per_jaar_2018').textContent = formatNumber(aantal_bedrijven_per_jaar_2018);
 	document.getElementById('aantal_bedrijven_per_jaar_2017').textContent = formatNumber(aantal_bedrijven_per_jaar_2017);
-	document.getElementById('aantal_bedrijven_per_jaar_2016').textContent = formatNumber(aantal_bedrijven_per_jaar_2016);
+	//document.getElementById('aantal_bedrijven_per_jaar_2016').textContent = formatNumber(aantal_bedrijven_per_jaar_2016);
 
+	var x2_aantal_bedrijven_per_jaar_2019 = aantal_bedrijven_per_jaar_2019 * 487 / aantal_bedrijven_per_jaar;
 	var x2_aantal_bedrijven_per_jaar_2018 = aantal_bedrijven_per_jaar_2018 * 487 / aantal_bedrijven_per_jaar;
 	var x2_aantal_bedrijven_per_jaar_2017 = aantal_bedrijven_per_jaar_2017 * 487 / aantal_bedrijven_per_jaar;
-	var x2_aantal_bedrijven_per_jaar_2016 = aantal_bedrijven_per_jaar_2016 * 487 / aantal_bedrijven_per_jaar;
+	//var x2_aantal_bedrijven_per_jaar_2016 = aantal_bedrijven_per_jaar_2016 * 487 / aantal_bedrijven_per_jaar;
 
+	document.getElementById('vulling_aantal_bedrijven_per_jaar_2019').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2019);
 	document.getElementById('vulling_aantal_bedrijven_per_jaar_2018').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2018);
 	document.getElementById('vulling_aantal_bedrijven_per_jaar_2017').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2017);
-	document.getElementById('vulling_aantal_bedrijven_per_jaar_2016').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2016);
+	//document.getElementById('vulling_aantal_bedrijven_per_jaar_2016').setAttribute('x2', x2_aantal_bedrijven_per_jaar_2016);
 
+	document.getElementById('aantal_bedrijven_per_jaar_2019').setAttribute('x', x2_aantal_bedrijven_per_jaar_2019 + 10);
 	document.getElementById('aantal_bedrijven_per_jaar_2018').setAttribute('x', x2_aantal_bedrijven_per_jaar_2018 + 10);
 	document.getElementById('aantal_bedrijven_per_jaar_2017').setAttribute('x', x2_aantal_bedrijven_per_jaar_2017 + 10);
-	document.getElementById('aantal_bedrijven_per_jaar_2016').setAttribute('x', x2_aantal_bedrijven_per_jaar_2016 + 10);
+	//document.getElementById('aantal_bedrijven_per_jaar_2016').setAttribute('x', x2_aantal_bedrijven_per_jaar_2016 + 10);
 
 	var aantal_bedrijven_per_kwartaal_1 = gegevens[regio].aantal_bedrijven_per_kwartaal.kwartaal_1;
 	var aantal_bedrijven_per_kwartaal_2 = gegevens[regio].aantal_bedrijven_per_kwartaal.kwartaal_2;
@@ -117,28 +122,28 @@ function maakGrafiek(regio) {
 	var aantal_zzp_ers_2019 = gegevens[regio].aantal_zzp_ers.jaar_2019;
 	document.getElementById('aantal_zzp_ers').textContent = formatNumber(aantal_zzp_ers_2019);
 
-	var aantal_werknemers_per_jaar = gegevens[regio].aantal_werknemers_per_jaar.jaar_2019;
+	var aantal_werknemers_per_jaar = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_1;;
 	document.getElementById('aantal_werknemers').textContent = formatNumber(aantal_werknemers_per_jaar);
 
+	var aantal_werknemers_per_jaar_2019 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2019;
 	var aantal_werknemers_per_jaar_2018 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2018;
 	var aantal_werknemers_per_jaar_2017 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2017;
-	var aantal_werknemers_per_jaar_2016 = gegevens[regio].aantal_werknemers_per_jaar.jaar_2016;
 
+	document.getElementById('aantal_werknemers_per_jaar_2019').textContent = formatNumber(aantal_werknemers_per_jaar_2019);
 	document.getElementById('aantal_werknemers_per_jaar_2018').textContent = formatNumber(aantal_werknemers_per_jaar_2018);
 	document.getElementById('aantal_werknemers_per_jaar_2017').textContent = formatNumber(aantal_werknemers_per_jaar_2017);
-	document.getElementById('aantal_werknemers_per_jaar_2016').textContent = formatNumber(aantal_werknemers_per_jaar_2016);
 
+	var x2_aantal_werknemers_per_jaar_2019 = aantal_werknemers_per_jaar_2019 * 487 / aantal_werknemers_per_jaar;
 	var x2_aantal_werknemers_per_jaar_2018 = aantal_werknemers_per_jaar_2018 * 487 / aantal_werknemers_per_jaar;
 	var x2_aantal_werknemers_per_jaar_2017 = aantal_werknemers_per_jaar_2017 * 487 / aantal_werknemers_per_jaar;
-	var x2_aantal_werknemers_per_jaar_2016 = aantal_werknemers_per_jaar_2016 * 487 / aantal_werknemers_per_jaar;
 
+	document.getElementById('vulling_aantal_werknemers_per_jaar_2019').setAttribute('x2', x2_aantal_werknemers_per_jaar_2019);
 	document.getElementById('vulling_aantal_werknemers_per_jaar_2018').setAttribute('x2', x2_aantal_werknemers_per_jaar_2018);
 	document.getElementById('vulling_aantal_werknemers_per_jaar_2017').setAttribute('x2', x2_aantal_werknemers_per_jaar_2017);
-	document.getElementById('vulling_aantal_werknemers_per_jaar_2016').setAttribute('x2', x2_aantal_werknemers_per_jaar_2016);
 
+	document.getElementById('aantal_werknemers_per_jaar_2019').setAttribute('x', x2_aantal_werknemers_per_jaar_2019 + 10);
 	document.getElementById('aantal_werknemers_per_jaar_2018').setAttribute('x', x2_aantal_werknemers_per_jaar_2018 + 10);
 	document.getElementById('aantal_werknemers_per_jaar_2017').setAttribute('x', x2_aantal_werknemers_per_jaar_2017 + 10);
-	document.getElementById('aantal_werknemers_per_jaar_2016').setAttribute('x', x2_aantal_werknemers_per_jaar_2016 + 10);
 
 	var aantal_werknemers_per_kwartaal_1 = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_1;
 	var aantal_werknemers_per_kwartaal_2 = gegevens[regio].aantal_werknemers_per_kwartaal.kwartaal_2;
