cf77ce1ef5bdaa9ab1b97a741ee91105781432c3
Johan van der Wijk <johan@vanderwijk.nl>
2021-05-07 12:30:31 +0200

    Cache

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-define('TRENDFILES_THEME_VER', '2.1.5');
+define('TRENDFILES_THEME_VER', '2.1.6');
 
 if (isset($_SERVER['HTTPS'])) {
 	$protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https://" : "http://";
